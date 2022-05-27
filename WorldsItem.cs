f4507c6f75808fc4532ef807bd7684217045b415
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-05-27 16:40:49 -0700

    Add files via upload

---
+++ WorldsItem.cs
@@ -0,0 +1,19 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using WorldsCollide.Items.Materials;
+
+namespace WorldsCollide
+{
+    public class WorldsItem : GlobalItem
+    {
+        public override void ExtractinatorUse(int extractType, ref int resultType, ref int resultStack)
+        {
+            if(extractType == 0)
+            {
+                if(Main.rand.NextBool(5))
+                resultType = ModContent.ItemType<Plastic>();
+                resultStack = 1;
+            }
+        }
+    }
+}
