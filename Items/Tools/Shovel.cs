3789b7990a6f126b71319c400de6afd63717b072
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-05-28 13:26:35 -0700

    Add files via upload

---
+++ Items/Tools/Shovel.cs
@@ -0,0 +1,43 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace WorldsCollide.Items.Tools
+{
+    public class Shovel : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Shovel");
+            Tooltip.SetDefault("Has a chance to reward gems when mining");
+        }
+		public override void SetDefaults()
+		{
+			Item.width = 30;
+			Item.height = 30;
+			//Item.value = 12000;
+			Item.rare = ItemRarityID.Blue;
+			Item.pick = 35;
+			Item.damage = 5;
+			Item.knockBack = 3;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.useTime = 16;
+			Item.useAnimation = 20;
+			Item.DamageType = DamageClass.Melee;
+			Item.useTurn = true;
+			Item.autoReuse = true;
+			Item.UseSound = SoundID.Item1;
+		}
+        public override void HoldItem(Player player)
+        {
+			WorldsPlayer modPlayer = player.GetWorldPlayer();
+			modPlayer.Pickaxe = true;
+		}
+
+
+
+
+
+
+    }
+}
