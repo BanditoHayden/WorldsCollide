f8f5019544f456fe20dc14f2ebbb81dad15f138f
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-05-28 14:27:09 -0700

    Add files via upload

---
+++ WorldsNpc.cs
@@ -0,0 +1,20 @@
+﻿using Terraria;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.ID;
+using Terraria.ModLoader;
+using WorldsCollide.Items.Materials;
+
+namespace WorldsCollide
+{
+    public class WorldsNpc : GlobalNPC
+    {
+        public override bool InstancePerEntity => true;
+        public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
+        {
+            if (npc.type == NPCID.Ghost)
+            {
+                npcLoot.Add(ItemDropRule.Common(ModContent.ItemType<GhostlyCloth>(), 5));
+            }
+        }
+    }
+}
