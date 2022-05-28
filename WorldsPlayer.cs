c1c47ef9b3b7b0b2af40c54cfe5f306895619d90
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-05-28 13:25:49 -0700

    Add files via upload

---
+++ WorldsPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameInput;
+using Terraria.Graphics.Shaders;
+using Terraria.GameContent.Events;
+using Terraria.ID;
+using Terraria.GameContent;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace WorldsCollide
+{
+    public class WorldsPlayer : ModPlayer
+    {
+        public bool Pickaxe = false;
+        private void ResetItems()
+        {
+            Pickaxe = false; 
+        }
+
+    }
+}
