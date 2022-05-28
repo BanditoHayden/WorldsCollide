83e13db89cc893ae75abb817fec3f83796941fdb
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-05-28 14:26:58 -0700

    Add files via upload

---
+++ Items/Materials/GhostlyCloth.cs
@@ -0,0 +1,24 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameContent.Creative;
+
+
+namespace WorldsCollide.Items.Materials
+{
+    public class GhostlyCloth : ModItem
+    {
+		public override void SetStaticDefaults()
+		{
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 99;
+			Tooltip.SetDefault("Spooky");
+		}
+		public override void SetDefaults()
+		{
+			Item.width = 42;
+			Item.height = 24;
+			Item.value = 200;
+			Item.rare = ItemRarityID.White;
+			Item.maxStack = 999;
+		}
+	}
+}
