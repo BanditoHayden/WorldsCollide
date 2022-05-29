650d58ff4e1d5fd58803515d215132d687ad5a41
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-05-29 14:10:13 -0700

    Add files via upload

---
+++ Projectiles/Ranged/NailProjectile.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.GameContent;
+using Terraria.Audio;
+
+namespace WorldsCollide.Projectiles.Ranged
+{
+	public class NailProjectile : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("NailProjectile");
+			ProjectileID.Sets.TrailCacheLength[Projectile.type] = 5;
+			ProjectileID.Sets.TrailingMode[Projectile.type] = 0;
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.width = 7;
+			Projectile.height = 7;
+			Projectile.penetrate = 1;
+			Projectile.aiStyle = 1;
+
+			AIType = ProjectileID.WoodenArrowFriendly;
+			Projectile.DamageType = DamageClass.Ranged;
+			Projectile.friendly = true;
+			
+		}
+		public override bool PreDraw(ref Color lightColor)
+		{
+			Main.instance.LoadProjectile(Projectile.type);
+			Texture2D texture = TextureAssets.Projectile[Projectile.type].Value;
+
+			// Redraw the projectile with the color not influenced by light
+			Vector2 drawOrigin = new Vector2(texture.Width * 0.5f, Projectile.height * 0.5f);
+			for (int k = 0; k < Projectile.oldPos.Length; k++)
+			{
+				Vector2 drawPos = (Projectile.oldPos[k] - Main.screenPosition) + drawOrigin + new Vector2(0f, Projectile.gfxOffY);
+				Color color = Projectile.GetAlpha(lightColor) * ((Projectile.oldPos.Length - k) / (float)Projectile.oldPos.Length);
+				Main.EntitySpriteDraw(texture, drawPos, null, color, Projectile.rotation, drawOrigin, Projectile.scale, SpriteEffects.None, 0);
+			}
+
+			return true;
+		}
+
+		public override void Kill(int timeLeft)
+		{
+			// This code and the similar code above in OnTileCollide spawn dust from the tiles collided with. SoundID.Item10 is the bounce sound you hear.
+			Collision.HitTiles(Projectile.position + Projectile.velocity, Projectile.velocity, Projectile.width, Projectile.height);
+			SoundEngine.PlaySound(SoundID.Item10, Projectile.position);
+		}
+	}
+}
