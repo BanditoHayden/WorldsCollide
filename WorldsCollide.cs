254943a2862f032e269ed0fc127aeaa30c04d92d
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-05-29 16:53:34 -0700

    Add files via upload

--- WorldsCollide.cs
+++ WorldsCollide.cs
@@ -1,8 +1,25 @@
 using Terraria.ModLoader;
+using Terraria.Graphics.Effects;
+using Terraria.Graphics.Shaders;
+using Terraria.ID;
+using Terraria;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
 
 namespace WorldsCollide
 {
 	public class WorldsCollide : Mod
 	{
-	}
+        public override void Load()
+        {
+            
+
+            if (Main.netMode != NetmodeID.Server)
+            {
+                Ref<Effect> screenRef = new Ref<Effect>(ModContent.Request<Effect>("WorldsCollide/Assets/Effects/ShockwaveEffect", AssetRequestMode.ImmediateLoad).Value);
+                Filters.Scene["Shockwave"] = new Filter(new ScreenShaderData(screenRef, "Shockwave"), EffectPriority.VeryHigh);
+                Filters.Scene["Shockwave"].Load();
+            }
+        }
+    }
 }
