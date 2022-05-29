9fe2ce0ce0872dd8eb5d75691ef4b8b99f7b7bbc
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-05-29 14:23:34 -0700

    Add files via upload

---
+++ Items/Weapons/Ranged/GhostlyBow.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+using WorldsCollide.Projectiles.Ranged;
+using WorldsCollide.Items.Ammo;
+using Terraria.Audio;
+using WorldsCollide.Items.Materials;
+
+namespace WorldsCollide.Items.Weapons.Ranged
+{
+    public class GhostlyBow : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+            DisplayName.SetDefault("Ghostly Bow");
+            Tooltip.SetDefault("");
+        }
+        public override void SetDefaults()
+        {
+            // Common Properties
+            Item.rare = ItemRarityID.Blue;
+            Item.width = 30;
+            Item.height = 30;
+            Item.scale = 1.0f;
+            // Use Properties
+            Item.useTime = 15;
+            Item.useAnimation = 15;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.noUseGraphic = false;
+            Item.UseSound = SoundID.Item5;
+            Item.autoReuse = false;
+            // Weapon Properties
+            Item.damage = 10;
+            Item.knockBack = 3.6f;
+            Item.DamageType = DamageClass.Ranged;
+            // Projectile Properties
+       
+            Item.shootSpeed = 5.7f;
+            Item.shoot = ProjectileID.WoodenArrowFriendly;
+            Item.useAmmo = AmmoID.Arrow;
+        }
+
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(-2, 0);
+        }
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback) {
+             if (type == ProjectileID.WoodenArrowFriendly) { // or ProjectileID.WoodenArrowFriendly
+                 type = ModContent.ProjectileType<GhostArrow>(); // or ProjectileID.FireArrow;
+             }
+         }
+
+
+
+    }
+}
---
+++ Items/Weapons/Ranged/NailGun.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+using WorldsCollide.Projectiles.Ranged;
+using WorldsCollide.Items.Ammo;
+using WorldsCollide.Items.Materials;
+
+namespace WorldsCollide.Items.Weapons.Ranged
+{
+    public class NailGun : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+            DisplayName.SetDefault("Nail-Gun");
+            Tooltip.SetDefault("");
+        }
+        public override void SetDefaults()
+        {
+            // Common Properties
+            Item.rare = ItemRarityID.Pink;
+            Item.width = 30;
+            Item.height = 30;
+            Item.scale = 1.0f;
+            // Use Properties
+            Item.useTime = 10;
+            Item.useAnimation = 10;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.noUseGraphic = false;
+            Item.UseSound = SoundID.Item108;
+            Item.autoReuse = true;
+            // Weapon Properties
+            Item.damage = 7;
+            Item.knockBack = 3.6f;
+            Item.DamageType = DamageClass.Ranged;
+            // Projectile Properties
+            Item.shoot = ModContent.ProjectileType<NailProjectile>();
+            Item.shootSpeed = 15.7f;
+            Item.useAmmo = ModContent.ItemType<BoxOfNails>();
+
+        }
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            Vector2 muzzleOffset = Vector2.Normalize(velocity) * 25f;
+            if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0))
+            {
+                position += muzzleOffset;
+            }
+        }
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(-2, 0);
+        }
+    }
+}
