e75ba873d38d0d3985be06366477d556cebe19c1
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-05-29 14:08:49 -0700

    Add files via upload

---
+++ Projectiles/Ranged/Hostile/GhostBlast.cs
@@ -0,0 +1,45 @@
+﻿using Terraria.GameContent.Creative;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using System;
+using WorldsCollide.Dusts;
+
+namespace WorldsCollide.Projectiles.Hostile
+{
+    public class GhostBlast : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+        }
+        public override void SetDefaults()
+        {
+            Projectile.width = 70;
+            Projectile.height = 70;
+            Projectile.aiStyle = -1;
+            Projectile.hostile = true;
+            Projectile.friendly = false;
+       
+            Projectile.timeLeft = 180;
+            Projectile.tileCollide = false;
+        }
+        public override void AI()
+        {
+            Projectile.velocity.Y += Projectile.ai[0];
+            if (Main.rand.NextBool(3))
+            {
+                Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height, ModContent.DustType<GhostDust>(), Projectile.velocity.X * 0.5f, Projectile.velocity.Y * 0.5f);
+            }
+            Projectile.direction = Projectile.spriteDirection = Projectile.velocity.X > 0f ? 1 : -1;
+            Projectile.rotation = Projectile.velocity.ToRotation();
+            if (Projectile.spriteDirection == -1)
+            {
+                Projectile.rotation += MathHelper.Pi;
+
+            }
+
+        }
+    }
+}
