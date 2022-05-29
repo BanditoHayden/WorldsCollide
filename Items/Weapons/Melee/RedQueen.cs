ff06331d99cfafaa4c33cdd8573a28abcae1e59a
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-05-29 14:23:15 -0700

    Add files via upload

---
+++ Items/Weapons/Melee/RedQueen.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace WorldsCollide.Items.Weapons.Melee
+{
+    public class RedQueen : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+        }
+        public override void SetDefaults()
+        {
+            // Common Properties
+            Item.rare = ItemRarityID.Red;
+            Item.width = 30;
+            Item.height = 30;
+            Item.scale = 1.0f;
+            // Use Properties
+            Item.useTime = 15;
+            Item.useAnimation = 15;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.noUseGraphic = false;
+            Item.UseSound = SoundID.Item18;
+            Item.autoReuse = false;
+            // Weapon Properties
+            Item.damage = 10;
+            Item.knockBack = 3.6f;
+            Item.DamageType = DamageClass.Melee;
+            // Projectile Properties
+
+        }
+        public override void OnHitNPC(Player player, NPC npc, int damage, float knockBack, bool crit)
+        {
+            npc.AddBuff(BuffID.OnFire, 360, false);
+        }
+        public override void MeleeEffects(Player player, Rectangle hitbox)
+        {
+            int dust = Dust.NewDust(new Vector2((float)hitbox.X, (float)hitbox.Y), hitbox.Width, hitbox.Height, 6, (player.velocity.X * 0.2f) + (player.direction * 3), player.velocity.Y * 0.2f, 100, default, 1.9f);
+            Main.dust[dust].noGravity = true;
+        }
+    }
+}
