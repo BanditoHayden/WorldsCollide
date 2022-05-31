f02776a5cf4ea9371349106e0fa11f77e3b38466
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-05-30 20:26:47 -0700

    Add files via upload

---
+++ Items/Vanity/DrFaust.cs
@@ -0,0 +1,30 @@
+﻿using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+namespace WorldsCollide.Items.Vanity
+{
+    [AutoloadEquip(EquipType.Head)]
+    public class DrFaust : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Dr.Faust");
+            Tooltip.SetDefault("");
+            ArmorIDs.Head.Sets.DrawHatHair[Item.headSlot] = true;
+           
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 18; // Width of the item
+            Item.height = 18; // Height of the item
+            Item.value = Item.sellPrice(gold: 1); // How many coins the item is worth
+            Item.rare = ItemRarityID.Green; // The rarity of the item
+            Item.vanity = true;
+        }
+    
+
+    }
+}
---
+++ Items/Vanity/Sonny/SonnyBoots.cs
@@ -0,0 +1,30 @@
+﻿using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+namespace WorldsCollide.Items.Vanity.Sonny
+{
+    [AutoloadEquip(EquipType.Legs)]
+    public class SonnyBoots : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Sonny's Boots");
+            Tooltip.SetDefault("");
+            
+
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 18; // Width of the item
+            Item.height = 18; // Height of the item
+            Item.value = Item.sellPrice(gold: 1); // How many coins the item is worth
+            Item.rare = ItemRarityID.Green; // The rarity of the item
+            Item.vanity = true;
+        }
+
+
+    }
+}
