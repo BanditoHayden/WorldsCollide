a245883a25c5c11cff627dbe72d5c9093429dc6b
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-05-30 20:23:15 -0700

    Add files via upload

---
+++ Items/Weapons/Melee/Vendetta.cs
@@ -0,0 +1,50 @@
+﻿using Terraria.Audio;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using Terraria;
+using Microsoft.Xna.Framework;
+using WorldsCollide.Projectiles.Melee;
+
+namespace WorldsCollide.Items.Weapons.Melee
+{
+    public class Vendetta : ModItem
+    {
+       
+        public override void SetStaticDefaults()
+        {
+
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+            Tooltip.SetDefault("");
+        }
+        public override void SetDefaults()
+        {
+            // Common Properties
+            Item.rare = ItemRarityID.Master;
+
+            Item.width = 60;
+            Item.height = 60;
+            Item.scale = 1.3f;
+            Item.noUseGraphic = false;
+            // Use Properties
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useAnimation = 40;
+            Item.useTime = 30;
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = false;
+            Item.scale = 1.4f;
+            // Weapon Properties
+            Item.damage = 10;
+            Item.knockBack = 8.6f;
+            Item.DamageType = DamageClass.Melee;
+            Item.crit = 14;
+            // Projectile Properties
+          
+        }
+
+
+        
+
+    }
+}
