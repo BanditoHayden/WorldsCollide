01cf2a929049fe8b75bdb54a9217ac20a2a86460
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-06-01 15:58:23 -0700

    Add files via upload

---
+++ Helpers.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Linq;
+using Terraria;
+using Terraria.Utilities;
+
+namespace WorldsCollide
+{
+    public static class Helpers
+    {
+        public static WorldsPlayer GetWorldPlayer(this Player player) => player.GetModPlayer<WorldsPlayer>();
+    }
+}
