80430fc3bf5496687b0257258b0cc2237bd9cdd1
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-06-01 15:54:05 -0700

    Add files via upload

--- Projectiles/Melee/YamatoSlash.cs
+++ Projectiles/Melee/YamatoSlash.cs
@@ -15,22 +15,20 @@ namespace WorldsCollide.Projectiles.Melee
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Yam");
-			ProjectileID.Sets.TrailCacheLength[Projectile.type] = 5;
-			ProjectileID.Sets.TrailingMode[Projectile.type] = 0;
+
 		}
 
 		public override void SetDefaults()
 		{
 			Projectile.width = 30;
 			Projectile.height = 30;
-			Projectile.penetrate = -1;
-			Projectile.aiStyle = 3;
+			Projectile.penetrate = 1;
+			Projectile.aiStyle = 0;
 			Projectile.tileCollide = false;
-			
 			Projectile.DamageType = DamageClass.Melee;
 			Projectile.friendly = true;
 			Projectile.timeLeft = 200;
-			
+			Projectile.light = 0.5f;	
 		}
 		public override void AI()
 		{
@@ -43,67 +41,27 @@ namespace WorldsCollide.Projectiles.Melee
 				float progress = (180f - Projectile.timeLeft) / 60f;
 				Filters.Scene["Shockwave"].GetShader().UseProgress(1).UseOpacity(500);
 			}
-			float maxDetectRadius = 800f; // The maximum radius at which a projectile can detect a target
-			float projSpeed = 5f; // The speed at which the projectile moves towards the target
-
-			// Trying to find NPC closest to the projectile
-			NPC closestNPC = FindClosestNPC(maxDetectRadius);
-			if (closestNPC == null)
-				return;
-
-			// If found, change the velocity of the projectile and turn it in the direction of the target
-			// Use the SafeNormalize extension method to avoid NaNs returned by Vector2.Normalize when the vector is zero
-			Projectile.velocity = (closestNPC.Center - Projectile.Center).SafeNormalize(Vector2.Zero) * projSpeed;
-			Projectile.rotation = Projectile.velocity.ToRotation();
 			
 		}
-
-		// Finding the closest NPC to attack within maxDetectDistance range
-		// If not found then returns null
-		public NPC FindClosestNPC(float maxDetectDistance)
-		{
-			NPC closestNPC = null;
-
-			// Using squared values in distance checks will let us skip square root calculations, drastically improving this method's speed.
-			float sqrMaxDetectDistance = maxDetectDistance * maxDetectDistance;
-
-			// Loop through all NPCs(max always 200)
-			for (int k = 0; k < Main.maxNPCs; k++)
-			{
-				NPC target = Main.npc[k];
-				// Check if NPC able to be targeted. It means that NPC is
-				// 1. active (alive)
-				// 2. chaseable (e.g. not a cultist archer)
-				// 3. max life bigger than 5 (e.g. not a critter)
-				// 4. can take damage (e.g. moonlord core after all it's parts are downed)
-				// 5. hostile (!friendly)
-				// 6. not immortal (e.g. not a target dummy)
-				if (target.CanBeChasedBy())
-				{
-					// The DistanceSquared function returns a squared distance between 2 points, skipping relatively expensive square root calculations
-					float sqrDistanceToTarget = Vector2.DistanceSquared(target.Center, Projectile.Center);
-
-					// Check if it is within the radius
-					if (sqrDistanceToTarget < sqrMaxDetectDistance)
-					{
-						sqrMaxDetectDistance = sqrDistanceToTarget;
-						closestNPC = target;
-					}
-				}
-			}
-
-			return closestNPC;
-		}
-
 		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
         {
-			
+			for (int i = 0; i < 10; i++)
+			{
+				Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height, ModContent.DustType<YamatoDust>(), Projectile.velocity.X * 0.5f, Projectile.velocity.Y * 0.5f);
+
+			}
+			Vector2 vel = Vector2.Zero;
+			Vector2 perturbedSpeed = Vector2.Zero;
+			//Projectile.NewProjectile(Projectile.GetSource_Death(), Projectile.position.X, Projectile.position.Y, perturbedSpeed.X, perturbedSpeed.Y, ModContent.ProjectileType<YamatoSlash2>(), 100, 0f, Projectile.owner);
+			Projectile.NewProjectile(Projectile.GetSource_Death(), target.position.X, target.position.Y, perturbedSpeed.X, perturbedSpeed.Y, ModContent.ProjectileType<YamatoSlash2>(), 100, 0f, Projectile.owner);
 		}
         public override void Kill(int timeLeft)
         {
 			SoundStyle style = new SoundStyle($"{nameof(WorldsCollide)}/Assets/Sounds/Items/Guns/Sheathe") with { Volume = .99f, };
 			SoundEngine.PlaySound(style);
+			
 			Filters.Scene["Shockwave"].Deactivate();
+			
 		}
 
 
