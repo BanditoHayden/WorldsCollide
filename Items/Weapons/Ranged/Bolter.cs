17439868388343d5945e38b51e82cee78c4341a5
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-06-03 02:40:40 -0700

    Add files via upload

---
+++ Items/Weapons/Ranged/Bolter.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+using WorldsCollide.Projectiles.Ranged;
+using WorldsCollide.Items.Ammo;
+using WorldsCollide.Items.Materials;
+
+namespace WorldsCollide.Items.Weapons.Ranged
+{
+    public class Bolter : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+            DisplayName.SetDefault("Bolter");
+            
+        }
+        public override void SetDefaults()
+        {
+            // Common Properties
+            Item.rare = ItemRarityID.Pink;
+            Item.width = 30;
+            Item.height = 30;
+            Item.scale = 0.8f;
+            // Use Properties
+            Item.useTime = 7;
+            Item.useAnimation = 7;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.noUseGraphic = false;
+            Item.UseSound = SoundID.Item11;
+            Item.autoReuse = true;
+            // Weapon Properties
+            Item.damage = 7;
+            Item.knockBack = 3.6f;
+            Item.DamageType = DamageClass.Ranged;
+            // Projectile Properties
+            Item.shoot = ProjectileID.Bullet;
+            Item.shootSpeed = 10.5f;
+            Item.useAmmo = AmmoID.Bullet;
+
+        }
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            Vector2 muzzleOffset = Vector2.Normalize(velocity) * 25f;
+            if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0))
+            {
+                position += muzzleOffset;
+                position.Y += -5f;
+            }
+        }
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(-2, 0);
+        }
+    }
+}
