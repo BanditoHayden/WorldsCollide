441c937757eb6fa68b3acde1e275c975fdb9f80d
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-06-04 03:59:24 -0700

    Add files via upload

---
+++ Items/Weapons/Melee/MohgsTrident.cs
@@ -0,0 +1,60 @@
+﻿
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+using WorldsCollide.Projectiles.Melee;
+
+namespace WorldsCollide.Items.Weapons.Melee
+{
+    public class MohgsTrident : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+
+            Tooltip.SetDefault("");
+
+            ItemID.Sets.SkipsInitialUseSound[Item.type] = true;
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            // Common Properties
+            // Item.rare = ModContent.RarityType<testrarity>();
+            Item.rare = 2;
+            Item.value = Item.sellPrice(silver: 10);
+            Item.width = 60;
+            Item.height = 60;
+            Item.noUseGraphic = true;
+            Item.UseSound = SoundID.Item71;
+            // Use Properties
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.autoReuse = true;
+            Item.useTime = 50;
+            Item.useAnimation = 50;
+            // Weapon Properties
+            Item.damage = 40;
+            Item.knockBack = 3.6f;
+            Item.DamageType = DamageClass.Melee;
+            // Projectile Properties
+            Item.shoot = ModContent.ProjectileType<MohgsTridentProjectile>();
+            Item.shootSpeed = 3.7f;
+        }
+        public override bool CanUseItem(Player player)
+        {
+            return player.ownedProjectileCounts[Item.shoot] < 1;
+        }
+        public override bool? UseItem(Player player)
+        {
+            // Because we're skipping sound playback on use animation start, we have to play it ourselves whenever the item is actually used.
+            if (!Main.dedServ && Item.UseSound.HasValue)
+            {
+                SoundEngine.PlaySound(Item.UseSound.Value, player.Center);
+            }
+
+            return null;
+        }
+    }
+}
