34faef6a87943139165cac976a545d9192c8b444
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-06-05 23:03:08 -0700

    Add files via upload

--- Projectiles/Melee/EternalFlamesProjectile.cs
+++ Projectiles/Melee/EternalFlamesProjectile.cs
@@ -7,7 +7,7 @@ using Terraria.ModLoader;
 using Terraria.Audio;
 using Terraria.GameContent;
 using WorldsCollide.Dusts;
-
+using System;
 
 namespace WorldsCollide.Projectiles.Melee
 {
@@ -16,20 +16,21 @@ namespace WorldsCollide.Projectiles.Melee
         public override void SetStaticDefaults()
         {
        
-            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 5;
-            ProjectileID.Sets.TrailingMode[Projectile.type] = 0;
+           
         }
         public override void SetDefaults()
         {
            Projectile.ignoreWater = false;
             Projectile.width = 40;
-            Projectile.penetrate = 1;
+            Projectile.penetrate = -1;
             Projectile.height = 40;
             Projectile.friendly = true;
             Projectile.light = 1f;
             Projectile.scale = 0.8f;
             Projectile.tileCollide = true;
             Projectile.aiStyle = 3;
+            AIType = 52;
+
         }
         public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
         {
@@ -38,25 +39,10 @@ namespace WorldsCollide.Projectiles.Melee
                 Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height, DustID.Flare, Projectile.velocity.X * 0.5f, Projectile.velocity.Y * 0.5f);
 
             }
+           
             target.AddBuff(BuffID.OnFire, 360, false);
         }
-        public override bool PreDraw(ref Color lightColor)
-        {
-            Main.instance.LoadProjectile(Projectile.type);
-            Texture2D texture = TextureAssets.Projectile[Projectile.type].Value;
-
-            // Redraw the projectile with the color not influenced by light
-            Vector2 drawOrigin = new Vector2(texture.Width * 0.5f, Projectile.height * 0.5f);
-            for (int k = 0; k < Projectile.oldPos.Length; k++)
-            {
-                Vector2 drawPos = (Projectile.oldPos[k] - Main.screenPosition) + drawOrigin + new Vector2(0f, Projectile.gfxOffY);
-                Color color = Projectile.GetAlpha(lightColor) * ((Projectile.oldPos.Length - k) / (float)Projectile.oldPos.Length);
-                Main.EntitySpriteDraw(texture, drawPos, null, color, Projectile.rotation, drawOrigin, Projectile.scale, SpriteEffects.None, 0);
-            }
-
-            return true;
-        }
-
+        
 
 
     }
