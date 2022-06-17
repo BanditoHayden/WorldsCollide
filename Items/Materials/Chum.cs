c0cfd3a2ce5b8ae157730c6c3e194dfae3dd2de5
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-06-17 07:12:17 -0700

    Add files via upload

---
+++ Items/Materials/Chum.cs
@@ -0,0 +1,25 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameContent.Creative;
+
+
+namespace WorldsCollide.Items.Materials
+{
+	public class Chum : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 99;
+		}
+		public override void SetDefaults()
+		{
+			Item.width = 44;
+			Item.height = 36;
+			Item.value = 200; // All of these need to be changed ahhhhhhh 
+			Item.rare = ItemRarityID.Blue;
+			Item.maxStack = 999;
+		}
+		
+
+	}
+}
