8fe1ba630b9eaa4fba207bac44a9243af5bbc3ae
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-06-17 06:31:41 -0700

    Slimed zombies drop slime dust now

--- Assets/Common/WorldsNpc.cs
+++ Assets/Common/WorldsNpc.cs
@@ -59,6 +59,11 @@ namespace WorldsCollide.Assets.Common
             {
                 npcLoot.Add(ItemDropRule.Common(ModContent.ItemType<SlimeDust>(), 5));
             }
+             if (npc.type == NPCID.SlimedZombie)
+            {
+                npcLoot.Add(ItemDropRule.Common(ModContent.ItemType<SlimeDust>(), 5));
+            }
+        
         }
     }
 }
