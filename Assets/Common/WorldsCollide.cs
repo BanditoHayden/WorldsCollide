61030a76f7faaf5473880b0839b0ad989d9f27d2
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-06-17 07:11:46 -0700

    more recipes

--- Assets/Common/WorldsCollide.cs
+++ Assets/Common/WorldsCollide.cs
@@ -20,6 +20,15 @@ namespace WorldsCollide.Assets.Common
                 Filters.Scene["Shockwave"].Load();
             }
         }
+	 public static RecipeGroup Fishes;
+        public override void AddRecipeGroups()
+        {
+            // Create a recipe group and store it
+            // Language.GetTextValue("LegacyMisc.37") is the word "Any" in english, and the corresponding word in other languages
+            Fishes = new RecipeGroup(() => $"{Language.GetTextValue("LegacyMisc.37")} {Lang.GetItemNameValue(ItemID.Fish)}",
+                ItemID.Bass, ItemID.CrimsonTigerfish,ItemID.Damselfish, ItemID.DoubleCod, ItemID.Ebonkoi, ItemID.Flounder, ItemID.GoldenCarp, ItemID.Hemopiranha, ItemID.Honeyfin, ItemID.NeonTetra, ItemID.RedSnapper, ItemID.Salmon, ItemID.Shrimp, ItemID.Trout, ItemID.Tuna);
+            RecipeGroup.RegisterGroup("WorldsCollide:Fishes", Fishes);
+        }
     	public override void AddRecipes()
         {
             CreateRecipe(ItemID.SlimeStaff, 1)
@@ -27,6 +36,11 @@ namespace WorldsCollide.Assets.Common
             .AddRecipeGroup(RecipeGroupID.Wood, 12)
             .AddTile(TileID.WorkBenches)
             .Register();
+	    
+	    CreateRecipe(ModContent.ItemType<Chum>(), 3)
+           .AddRecipeGroup("WorldsCollide:Fishes", 1)
+           .AddTile(TileID.Extractinator)
+           .Register();
         }  
     }
 }
