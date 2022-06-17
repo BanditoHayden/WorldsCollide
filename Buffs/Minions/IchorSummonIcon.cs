30647bb8ddddd9a510bb9d002da325894652306e
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-06-16 19:41:46 -0700

    Add files via upload

---
+++ Buffs/Minions/IchorSummonIcon.cs
@@ -0,0 +1,38 @@
+﻿
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+using WorldsCollide.Projectiles.Summon;
+
+namespace WorldsCollide.Buffs.Minions
+{
+	public class IchorSummonIcon : ModBuff
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Ghost Minion");
+			Description.SetDefault("The Ghost will fight for you");
+
+			Main.buffNoSave[Type] = true; // This buff won't save when you exit the world
+			Main.buffNoTimeDisplay[Type] = true; // The time remaining won't display on this buff
+		}
+
+		public override void Update(Player player, ref int buffIndex)
+		{
+			// If the minions exist reset the buff time, otherwise remove the buff from the player
+			if (player.ownedProjectileCounts[ModContent.ProjectileType<IchorSummon>()] > 0)
+			{
+				player.buffTime[buffIndex] = 18000;
+			}
+			else
+			{
+				player.DelBuff(buffIndex);
+				buffIndex--;
+			}
+		}
+	}
+}
