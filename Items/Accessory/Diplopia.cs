6dbe1a32cbbe3204d1f957172701f426a155618b
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-06-21 16:19:17 -0700

    Add files via upload

---
+++ Items/Accessory/Diplopia.cs
@@ -0,0 +1,28 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using WorldsCollide.Assets.Common;
+
+namespace WorldsCollide.Items.Accessory
+{
+    public class Diplopia : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Your seeing double!\nOre drops are now doubled!");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.accessory = true;
+            Item.value = Item.sellPrice(0, 1, 0, 0);
+            Item.rare = 2;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.GetModPlayer<WorldsPlayer>().Diplopia = true;
+        }
+    }
+}
