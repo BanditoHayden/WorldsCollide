5f945a9d1c8a31176acc423a8b778df1eb52ab21
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-06-21 16:05:49 -0700

    Diplopia Part 2
    Thank you Volcanic!

--- Assets/Common/WorldsTile.cs
+++ Assets/Common/WorldsTile.cs
@@ -23,5 +23,38 @@ namespace WorldsCollide.Assets.Common
                 }
             }
         }
+       public override bool Drop(int i, int j, int type)
+        {
+			Player player = Main.LocalPlayer;
+			WorldsPlayer modPlayer = player.GetWorldPlayer();
+			if (TileID.Sets.Ore[type] && modPlayer.Diplopia)
+            {
+				DropTheGoods(i, j, type);
+			}
+			return true;
+		}
+		public void DropTheGoods(int i, int j, int type)
+		{
+			ModTile modTile = TileLoader.GetTile(type);
+			if (modTile == null)
+			{
+				if (TileID.Sets.Ore[type] && WorldsCollide.oreTileToItem.TryGetValue(type, out int item))
+				{
+					Item.NewItem(new EntitySource_TileBreak(i, j), i * 16, j * 16, 32, 32, item, Stack: 1);
+				}
+			}
+			else
+			{
+				if (TileID.Sets.Ore[type])
+				{
+					int drop = modTile.ItemDrop;
+					if (drop > 0)
+					{
+						Item.NewItem(WorldGen.GetItemSource_FromTileBreak(i, j), i * 16, j * 16, 16, 16, drop, 1);
+					}
+				}
+			}
+
+		}
     }
 }
