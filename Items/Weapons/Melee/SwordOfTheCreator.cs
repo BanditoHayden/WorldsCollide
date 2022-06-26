d0ec8df54f199d556ca26503b3abf6905c33a5fe
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-06-26 16:40:37 -0700

    Add files via upload

---
+++ Items/Weapons/Melee/SwordOfTheCreator.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using WorldsCollide.Projectiles.Melee;
+
+namespace WorldsCollide.Items.Weapons.Melee
+{
+    public class SwordOfTheCreator : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+
+        }
+        public override void SetDefaults()
+        {
+            // Common Properties
+            Item.rare = ItemRarityID.Red;
+            Item.width = 30;
+            Item.height = 30;
+            Item.scale = 1.0f;
+            // Use Properties
+            Item.useTime = 15;
+            Item.useAnimation = 15;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.noUseGraphic = false;
+            Item.UseSound = SoundID.Item18;
+            Item.autoReuse = false;
+            // Weapon Properties
+            Item.damage = 19;
+            Item.knockBack = 3f;
+            Item.DamageType = DamageClass.Melee;
+            // Projectile Properties
+
+        }
+        public override bool AltFunctionUse(Player player)
+        {
+            return true;
+        }
+        public override bool CanUseItem(Player player)
+        {
+            if (player.altFunctionUse == 2)
+            {
+                Item.shoot = ModContent.ProjectileType<CreatorWhip>();
+                Item.noUseGraphic = true;
+                Item.noMelee = true;
+                Item.DefaultToWhip(ModContent.ProjectileType<CreatorWhip>(), 20, 2, 4);
+                Item.channel = true;
+            }
+            else
+            {
+                Item.noUseGraphic = false;
+                Item.noMelee = false;
+
+            }
+            return base.CanUseItem(player);
+        }
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            if (player.altFunctionUse == 2)
+            {
+                Projectile.NewProjectile(source, position, velocity, ModContent.ProjectileType<CreatorWhip>(), damage, knockback, player.whoAmI);
+            }
+            return false;
+
+        }
+    }
+
+}
