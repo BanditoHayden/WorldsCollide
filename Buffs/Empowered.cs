21facfdaa253aee453c09257a99ae68e98f83365
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-07-05 01:08:58 -0700

    Add files via upload

---
+++ Buffs/Empowered.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using System.Threading.Tasks;
+
+namespace WorldsCollide.Buffs
+{
+    public class Empowered : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Empowered");
+            Description.SetDefault("Increased damage");
+            Main.buffNoTimeDisplay[Type] = false;
+            Main.debuff[Type] = true;
+        }
+
+      
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.GetDamage(DamageClass.Melee) += 0.10f;
+        }
+
+
+    }
+
+}
