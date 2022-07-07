c42bebc91501bef304a26a1d98629d8f5fa3a2c9
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-07-06 22:48:52 -0700

    Add files via upload

---
+++ Items/Accessory/BumBullets.cs
@@ -0,0 +1,29 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using WorldsCollide.Assets.Common;
+
+namespace WorldsCollide.Items.Accessory
+{
+    public class BumBullets : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Bumblecore\nShooting occasionally spawns additional bees.");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.accessory = true;
+            Item.value = Item.sellPrice(0, 1, 0, 0);
+            Item.rare = ItemRarityID.Green;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.GetModPlayer<WorldsPlayer>().Bumble = true;
+        }
+        
+    }
+}
