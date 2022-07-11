e4f7b11ba86c16bd5eff0ce77de7c6179381974a
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-07-11 13:38:31 -0700

    Add files via upload

---
+++ Items/Weapons/Melee/GreatShadewoodSword.cs
@@ -0,0 +1,43 @@
+﻿
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+using WorldsCollide.Projectiles.Melee;
+
+namespace WorldsCollide.Items.Weapons.Melee
+{
+    public class GreatShadewoodSword : ModItem
+    {
+        int currentAttack = 1;
+        public override void SetStaticDefaults()
+        {
+        }
+        public override void SetDefaults()
+        {
+            // Common Properties
+            Item.rare = ItemRarityID.Green;
+
+            Item.width = 50;
+            Item.height = 50;
+            Item.scale = 1f;
+            // Use Properties
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useAnimation = 20;
+            Item.useTime = 20;
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = false;
+            // Weapon Properties
+            Item.damage = 17;
+            Item.knockBack = 5f;
+            Item.DamageType = DamageClass.Melee;
+            // Projectile Properties
+
+        }
+
+
+    }
+}
