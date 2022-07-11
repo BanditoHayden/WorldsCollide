6874a3df38ee39b2cdb0ca9bf78da0bcfe758a7e
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-07-11 14:44:02 -0700

    Add files via upload

---
+++ Items/Weapons/Ranged/ShadewoodStrike.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+using WorldsCollide.Projectiles.Ranged;
+using WorldsCollide.Items.Ammo;
+using Terraria.Audio;
+using WorldsCollide.Items.Materials;
+
+namespace WorldsCollide.Items.Weapons.Ranged
+{
+    public class ShadewoodStrike : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+        public override void SetDefaults()
+        {
+            // Common Properties
+            Item.rare = ItemRarityID.Blue;
+            Item.width = 30;
+            Item.height = 30;
+            // Use Properties
+            Item.useTime = 25;
+            Item.useAnimation = 25;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.UseSound = SoundID.Item5;
+            Item.autoReuse = true;
+            Item.crit = 8;
+            // Weapon Properties
+            Item.damage = 17;
+            Item.knockBack = 3f;
+            Item.DamageType = DamageClass.Ranged;
+            // Projectile Properties
+            
+            Item.shootSpeed = 7f;
+            Item.shoot = ProjectileID.WoodenArrowFriendly;
+            Item.useAmmo = AmmoID.Arrow;
+        }
+
+
+       
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+           .AddIngredient(ItemID.CrimtaneBar, 8)
+           .AddIngredient(ItemID.ShadewoodBow, 1)
+           .AddTile(TileID.WorkBenches)
+           .Register();
+        }
+
+
+    }
+}
